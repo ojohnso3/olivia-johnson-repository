d43531c497c69d58fcda50a92780c123b8aa0eee
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2020-09-17 16:10:48 -0400

    Fix antd packagejson and added more components
    Brought antd back to previous version and added new js and css files for projects

---
+++ src/components/Carousel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../css/Carousel.css';
+import { Modal } from 'antd';
+import { LeftOutlined, RightOutlined} from '@ant-design/icons';
+
+class Carousel extends React.Component {
+  
+  constructor(props) {
+    super(props); 
+
+  }
+
+  render() {
+    return (
+      <div className='carousel-wrapper'>
+        <h1 id='carousel-title'>{this.props.title}</h1>
+
+        <div className='project-container'>
+          <LeftOutlined style={{ fontSize: '100px'}} className='arrow'/>
+          <div className='project-item' onClick={()=>this.props.chooseModal(this.props.item1['short-title'])}>
+            <img src={this.props.item1['image']} className='project-image'></img>
+            <h3 className='project-name'>{this.props.item1['short-title']}</h3>
+          </div>
+          <div className='project-item' onClick={()=>this.props.chooseModal(this.props.item2['short-title'])}>
+            <img src={this.props.item2['image']} className='project-image'></img>
+            <h3 className='project-name'>{this.props.item2['short-title']}</h3>
+          </div>
+
+          { this.props.item3 == 'no_item' ?
+              null
+              :
+              <div className='project-item' onClick={()=>this.props.chooseModal(this.props.item3['short-title'])}>
+                <img src={this.props.item3['image']} className='project-image'></img>
+                <h3 className='project-name'>{this.props.item3['short-title']}</h3>
+              </div>
+          }
+          <RightOutlined style={{ fontSize: '100px'}} className='arrow'/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Carousel;
